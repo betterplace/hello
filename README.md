bc69ca0531200465b55c201426ad8737af0a76da
Florian Frank <flori@ping.de>
2022-02-14 20:50:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello, Betterplace
+
+test app
---
+++ cmd/hello/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	hello "github.com/betterplace/betterplace-hello"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	var config hello.Config
+	err := envconfig.Process("", &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, Betterplace!\n")
+	})
+
+	e.Logger.Fatal(e.Start(":" + config.PORT))
+}
